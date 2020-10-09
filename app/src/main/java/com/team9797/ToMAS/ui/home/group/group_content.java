fda7ec417a51274c1cf8931a21498c5e0ef45f85
rlarla915 <rlarla915@gmail.com>
2020-10-09 19:25:51 +0900

    인원모집 기능에 User 정보 연동 중 -미완

--- app/src/main/java/com/team9797/ToMAS/ui/home/group/group_content.java
+++ app/src/main/java/com/team9797/ToMAS/ui/home/group/group_content.java
@@ -5,7 +5,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,8 +20,13 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.team9797.ToMAS.MainActivity;
 import com.team9797.ToMAS.R;
+import com.team9797.ToMAS.fragment_template;
+
+import java.util.ArrayList;
 
 public class group_content extends Fragment {
 
@@ -30,6 +38,7 @@ public class group_content extends Fragment {
     TextView time_textView;
     TextView numpeople_textView;
     Button btn_enroll ;
+    GridView participant_gridView;
 
     String path;
     String post_id;
@@ -55,6 +64,10 @@ public class group_content extends Fragment {
         time_textView = root.findViewById(R.id.group_content_time);
         numpeople_textView = root.findViewById(R.id.group_content_numpeople);
         btn_enroll = root.findViewById(R.id.group_content_enroll);
+        participant_gridView = root.findViewById(R.id.participant_list);
+
+        final participant_list_adapter adapter = new participant_list_adapter();
+        participant_gridView.setAdapter(adapter);
 
         // 선택한 게시물 document reference
         DocumentReference mPostReference = mainActivity.db.collection(path).document(post_id);
@@ -70,6 +83,11 @@ public class group_content extends Fragment {
                         date_textView.setText(document.get("date", String.class));
                         time_textView.setText(document.get("time", String.class));
                         numpeople_textView.setText(Integer.toString(document.get("now_people", Integer.class))+"/" + Integer.toString(document.get("num_people", Integer.class)));
+                        ArrayList<String> tmp_participants = document.get("participants", ArrayList.class);
+                        for (int i = 0; i<tmp_participants.size(); i++)
+                        {
+                            adapter.addItem(tmp_participants.get(i));
+                        }
                     } else {
 
                     }
@@ -78,6 +96,45 @@ public class group_content extends Fragment {
                 }
             }
         });
+
+        /*
+        * 참고용 코드 (지워야함)
+        // 구조 다시 바꿈. fragment_template에서 child_list를 firebase에서 읽어옴.
+        // firebase에서 child_list채우기
+        mainActivity.db.collection(path).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    child_list.clear();
+                    child_fragment_list.clear();
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        //Log.d(TAG, document.getId() + " => " + document.getData());
+                        child_list.add(document.getId());
+                        child_fragment_list.add(document.get("fragment_style", Integer.class));
+                    }
+                    listview_adapter.notifyDataSetChanged();
+                } else {
+                    //Log.d(TAG, "Error getting documents: ", task.getException());
+                }
+            }
+        });
+        tmp_listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.addToBackStack(null);
+                // 다음 child를 만들고 arg 넘기는 과정
+                // need to fix db에서 받아와서 분기해야댐.
+                Fragment change_fragment = new fragment_template();
+                Bundle args = new Bundle();
+                args.putInt("fragment_style", child_fragment_list.get(i));
+                args.putString("title", child_list.get(i));
+                args.putString("path", path);
+                change_fragment.setArguments(args);
+                fragmentTransaction.replace(R.id.nav_host_fragment, change_fragment).commit();
+            }
+        });
+        */
 
         // user id가 participation 목록에 있으면 버튼 text를 취소로 바꾸기.
         /*
---
+++ app/src/main/java/com/team9797/ToMAS/ui/home/group/participant_list_item.java
@@ -0,0 +1,25 @@
+package com.team9797.ToMAS.ui.home.group;
+
+public class participant_list_item {
+    private String phone;
+    private String name ;
+
+
+
+
+    public void setName(String a) {
+        name = a ;
+    }
+    public void setPhone(String a) {
+        phone = a ;
+    }
+
+
+    public String getPhone() {
+        return this.phone;}
+    public String getName() {
+        return this.name ;
+    }
+
+
+}
