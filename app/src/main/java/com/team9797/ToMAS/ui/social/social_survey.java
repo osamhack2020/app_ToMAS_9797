05412caf1c596326b2fcc730ed0b96fb76018049
rlarla915 <rlarla915@gmail.com>
2020-10-17 10:08:03 +0900

    플리마켓 구현 + 설문조사 구현 중

---
+++ app/src/main/java/com/team9797/ToMAS/ui/social/social_survey.java
@@ -0,0 +1,51 @@
+package com.team9797.ToMAS.ui.social;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.team9797.ToMAS.MainActivity;
+import com.team9797.ToMAS.R;
+
+public class SocialFragment extends Fragment {
+
+    MainActivity mainActivity;
+    TextView survey_textView;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_social, container, false);
+        mainActivity = (MainActivity)getActivity();
+        fragmentManager = getFragmentManager();
+
+        survey_textView = root.findViewById(R.id.social_survey);
+        survey_textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment change_fragment = ;
+
+                fragmentTransaction = fragmentManager.beginTransaction();
+
+            }
+        });
+        return root;
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        mainActivity.set_title();
+    }
+}
