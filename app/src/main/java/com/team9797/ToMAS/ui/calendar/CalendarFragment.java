edafb89168705659a9a6f334700e6f515b409ff3
rlarla915 <rlarla915@gmail.com>
2020-10-24 18:11:25 +0900

    캘린더 라이브러리 교체

--- app/src/main/java/com/team9797/ToMAS/ui/calendar/CalendarFragment.java
+++ app/src/main/java/com/team9797/ToMAS/ui/calendar/CalendarFragment.java
@@ -7,6 +7,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateLongClickListener;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,9 +22,7 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
-import com.applandeo.materialcalendarview.CalendarView;
-import com.applandeo.materialcalendarview.EventDay;
-import com.applandeo.materialcalendarview.listeners.OnDayClickListener;
+import com.prolificinteractive.materialcalendarview.OnRangeSelectedListener;
 import com.team9797.ToMAS.MainActivity;
 import com.team9797.ToMAS.R;
 
@@ -27,64 +30,39 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-public class CalendarFragment extends Fragment {
+public class CalendarFragment extends Fragment implements OnRangeSelectedListener{
 
 
-    CalendarView calendarView;
+    MaterialCalendarView calendarView;
     MainActivity mainActivity;
     Button btn_calendar;
+    RangeDayDecorator decorator;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_calendar, container, false);
         mainActivity = (MainActivity)getActivity();
-        calendarView = (CalendarView) root.findViewById(R.id.calendarView);
+        calendarView = root.findViewById(R.id.calendarView);
         btn_calendar = root.findViewById(R.id.btn_calendar);
+        decorator = new RangeDayDecorator(mainActivity);
+        calendarView.setOnRangeSelectedListener(this);
+        calendarView.addDecorator(decorator);
 
-    /*
-        calendarView.setOnDayClickListener(new OnDayClickListener() {
-            @Override
-            public void onDayClick(EventDay eventDay) {
-                List<Calendar> selectedDates = calendarView.getSelectedDates();
-                if (selectedDates.size() == 1)
-                {
-                    Log.d("QQQ", "one clicked");
-                }
-                else
-                {
-                    Log.d("QQQ", "many clicked");
-                }
-                for (int i = 0; i< selectedDates.size(); i++)
-                    Log.d("AAA", selectedDates.get(i).toString());
-            }
-        });
-
-     */
-        List<EventDay> events = new ArrayList<>();
-        Calendar calendar1 = Calendar.getInstance();
-        calendar1.add(Calendar.DAY_OF_MONTH, 2);
-        events.add(new EventDay(calendar1, R.drawable.sample_icon_2));
-        calendarView.setEvents(events);
-        /*
-        btn_calendar.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                List<Calendar> selectedDates = calendarView.getSelectedDates();
-                List<EventDay> events = new ArrayList<>();
-                Log.d("QQ", Integer.toString(selectedDates.size()));
-                for (int i = 0; i< selectedDates.size(); i++) {
-                    events.add(new EventDay(selectedDates.get(i), R.drawable.sample_icon_2));
-                    Log.d("QQ", "AA");
-                }
-                calendarView.setEvents(events);
-                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-                fragmentTransaction.detach(CalendarFragment.this).attach(CalendarFragment.this).commit();
-            }
-        });
-
-         */
 
         return root;
+    }
+
+    @Override
+    public void onRangeSelected(@NonNull MaterialCalendarView widget, @NonNull List<CalendarDay> dates) {
+
+        if (dates.size() > 0) {
+            for (int i = 0; i < dates.size(); i++)
+            {
+                decorator.
+            }
+            calendarView.invalidateDecorators();
+        }
+
     }
 }
