472301a1d4806659a01c2ab6942917f8c4bbf3e0
rlarla915 <rlarla915@gmail.com>
2020-10-25 15:40:36 +0900

    소속트리 구조 변경 및 선택 시 색상 추가

--- app/src/main/java/com/team9797/ToMAS/ui/home/market/belong_tree/belong_tree_dialog.java
+++ app/src/main/java/com/team9797/ToMAS/ui/home/market/belong_tree/belong_tree_dialog.java
@@ -6,6 +6,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -43,6 +44,9 @@ public class belong_tree_dialog extends DialogFragment {
     TreeViewAdapter adapter;
     String last_select;
     tree_dialog_result mdialogResult;
+    List<TreeNode> nodes;
+    boolean check_first;
+    RecyclerView.ViewHolder before_holder = null;
 
     public interface tree_dialog_result{
         void get_result(String result);
@@ -62,6 +66,33 @@ public class belong_tree_dialog extends DialogFragment {
         context = tmp_context;
     }
 
+
+    public void init_tree(TreeNode<Dir> node)
+    {
+        // 자기자신의 path까지 node로 저장하고 이를 firebase path에 넘겨줌.
+        FirebaseFirestore.getInstance().collection(node.getPath())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                // 소속 tree에는 market이나 중대게시판 안보이게 처리
+                                String tmp_path = node.getPath() + "/" + document.getId() + "/" + document.getId();
+                                TreeNode<Dir> tmp_node = new TreeNode<>(new Dir(document.getId()), tmp_path);
+                                init_tree(tmp_node);
+                                node.addChild(tmp_node);
+                            }
+                            if (check_first) {
+                                adapter.refresh(nodes);
+                                check_first = false;
+                            }
+                        } else {
+                        }
+                    }
+                });
+    }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
@@ -73,61 +104,37 @@ public class belong_tree_dialog extends DialogFragment {
         // get view
         rv = rootView.findViewById(R.id.belong_tree_recyclerview);
 
-        // init tree
-        List<TreeNode> nodes = new ArrayList<>();
+        nodes = new ArrayList<>();
         TreeNode<Dir> app = new TreeNode<>(new Dir("소속"), "armyunit");
         nodes.add(app);
+        check_first = true;
+        init_tree(app);
+
         rv.setLayoutManager(new LinearLayoutManager(context));
         adapter = new TreeViewAdapter(nodes, Arrays.asList(new FileNodeBinder(), new DirectoryNodeBinder()));
         // whether collapse child nodes when their parent node was close.
 //        adapter.ifCollapseChildWhileCollapseParent(true);
-        adapter.setOnTreeNodeListener(new TreeViewAdapter.OnTreeNodeListener() {
 
+        adapter.setOnTreeNodeListener(new TreeViewAdapter.OnTreeNodeListener() {
             @Override
             public boolean onClick(TreeNode node, RecyclerView.ViewHolder holder) {
-                Log.d("AAA", "QQQQQQQ");
                 last_select = node.getPath();
                 //Update and toggle the node.
                 onToggle(!node.isExpand(), holder);
-//                    if (!node.isExpand())
-//                        adapter.collapseBrotherNode(node);
-                // 이미 child가 추가된 경우에는 서버에서 불러오지 않음.
-                if (!node.isLeaf())
-                    return false;
-                // 자기자신의 path까지 node로 저장하고 이를 firebase path에 넘겨줌.
-                FirebaseFirestore.getInstance().collection(node.getPath())
-                        .get()
-                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                            @Override
-                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                if (task.isSuccessful()) {
-                                    for (QueryDocumentSnapshot document : task.getResult()) {
-                                        // 소속 tree에는 market이나 중대게시판 안보이게 처리
-                                        if (document.getId() == "market" || document.getId() == "notice")
-                                            continue;
-                                        Log.d("AAA", document.getId());
-                                        String tmp_path = node.getPath() + "/" + document.getId() + "/" + document.getId();
-                                        node.addChild(new TreeNode<>(new Dir(document.getId()), tmp_path));
-                                    }
-                                    //adapter.notifyDataSetChanged();
-                                    node.expand();
-                                    adapter.refresh(nodes);
-                                    //adapter.refresh(nodes);
-
-                                } else {
-                                    //Log.d(TAG, "Error getting documents: ", task.getException());
-                                }
-                            }
-                        });
-
-
-
+                if (before_holder != null)
+                {
+                    Log.d("AA","brforeHolder is not null");
+                    before_holder.itemView.setBackgroundColor(Color.parseColor("#00FFFFFF"));
+                }
+                before_holder = holder;
+                holder.itemView.setBackgroundColor(Color.parseColor("#FFEB3B"));
                 return false;
             }
 
             @Override
             public void onToggle(boolean isExpand, RecyclerView.ViewHolder holder) {
                 DirectoryNodeBinder.ViewHolder dirViewHolder = (DirectoryNodeBinder.ViewHolder) holder;
+                dirViewHolder.itemView.setBackgroundColor(Color.YELLOW);
                 final ImageView ivArrow = dirViewHolder.getIvArrow();
                 int rotateDegree = isExpand ? 90 : -90;
                 ivArrow.animate().rotationBy(rotateDegree)
