c69e4b7b20a3d108d48d6e72267cca8ee5521da3
rlarla915 <rlarla915@gmail.com>
2020-10-27 02:48:56 +0900

    캘린더 기능 구현 중

--- app/src/main/java/com/team9797/ToMAS/ui/calendar/CalendarFragment.java
+++ app/src/main/java/com/team9797/ToMAS/ui/calendar/CalendarFragment.java
@@ -7,6 +7,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateLongClickListener;
@@ -25,19 +31,28 @@ import androidx.lifecycle.ViewModelProviders;
 import com.prolificinteractive.materialcalendarview.OnRangeSelectedListener;
 import com.team9797.ToMAS.MainActivity;
 import com.team9797.ToMAS.R;
+import com.team9797.ToMAS.ui.home.market.belong_tree.belong_tree_dialog;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class CalendarFragment extends Fragment implements OnRangeSelectedListener{
+public class CalendarFragment extends Fragment implements OnDateSelectedListener, OnRangeSelectedListener{
 
 
     MaterialCalendarView calendarView;
     MainActivity mainActivity;
     Button btn_calendar;
+    TextView type_textView;
+    TextView title_textView;
+    TextView content_textView;
     RangeDayDecorator decorator;
-    List<CalendarDay> selected_dayList;
+    List<CalendarDay> selected_day_list = new ArrayList<>();
+    ArrayList<List<CalendarDay>> events = new ArrayList<>();
+    HashMap<CalendarDay, event> day_to_event_map = new HashMap<>();
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -50,21 +65,96 @@ public class CalendarFragment extends Fragment implements OnRangeSelectedListene
         calendarView.setOnRangeSelectedListener(this);
         calendarView.addDecorator(decorator);
 
+        mainActivity.db.collection("user").document(mainActivity.getUid()).collection("events")
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        // calendarDay를 firebase내에 저장 할 수 있는지 확인 후 적용
+                        event tmp_event = new event(document.get("type").toString(), document.get("title").toString(), document.get("content").toString(), document.get("color", Integer.class), document.getId());
+                        ArrayList<CalendarDay> tmp_list= (ArrayList<CalendarDay>)document.get("days");
+                        for (int i = 0; i< tmp_list.size(); i++)
+                        {
+                            day_to_event_map.put(tmp_list.get(i), tmp_event);
+                        }
+                        events.add(selected_day_list);
+                        //show_events(color);
+                    }
+                } else {
+                    //Log.d(TAG, "Error getting documents: ", task.getException());
+                }
+            }
+        });
+
         btn_calendar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // 일정추가에서 받은 날짜들을 그려준다.
-
-                calendarView.invalidateDecorators();
-
+                add_event_dialog dialog = new add_event_dialog(mainActivity);
+                dialog.show(mainActivity.getSupportFragmentManager(), "일정등록");
+                dialog.setDialogResult(new add_event_dialog.add_event_dialog_result() {
+                    @Override
+                    public void get_result(String type, String title, String content, int color) {
+                        // 여기서 서버로 보내기
+                        Map<String, Object> post = new HashMap<>();
+                        post.put("type", type);
+                        post.put("title", title);
+                        post.put("content", content);
+                        post.put("color", color);
+                        post.put("days", selected_day_list); // 이거 안되면 for문 돌려서 calendarDay 변환해서 firebase 넣기
+                    }
+                });
             }
         });
 
         return root;
     }
 
+    public void show_events()
+    {
+        for (int i = 0; i < events.size(); i++)
+        {
+            // 여기서 글자 추가하면 될 듯
+            calendarView.addDecorator(new EventDecorator(day_to_event_map.get(events.get(i).get(0)).color, events.get(i)));
+        }
+        calendarView.invalidateDecorators();
+    }
+
     @Override
     public void onRangeSelected(@NonNull MaterialCalendarView widget, @NonNull List<CalendarDay> dates) {
-        selected_dayList = dates;
+        selected_day_list.clear();
+        for (int i = 0; i < dates.size(); i++)
+            selected_day_list.add(dates.get(i));
+    }
+
+    @Override
+    public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+        Log.d("AA", "selected!!!");
+        event selected_event = day_to_event_map.get(date);
+        type_textView.setText(selected_event.type);
+        title_textView.setText(selected_event.title);
+        content_textView.setText(selected_event.content);
+
+    }
+}
+
+class event{
+    String type;
+    String title;
+    String content;
+    int color;
+    String id;
+
+    event()
+    {
+
+    }
+    event(String tmp_type, String tmp_title, String tmp_content, int tmp_color, String tmp_id)
+    {
+        type = tmp_type;
+        title = tmp_title;
+        content = tmp_content;
+        color = tmp_color;
+        id = tmp_id;
     }
 }
---
+++ app/src/main/java/com/team9797/ToMAS/ui/calendar/add_event_dialog.java
@@ -0,0 +1,147 @@
+package com.team9797.ToMAS.ui.calendar;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.team9797.ToMAS.R;
+import com.team9797.ToMAS.postBoard.register_board_content;
+import com.team9797.ToMAS.ui.home.market.register_market_content;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import top.defaults.colorpicker.ColorPickerPopup;
+
+
+public class add_event_dialog extends DialogFragment {
+
+    Context context;
+    add_event_dialog_result mdialogResult;
+    int selected_color;
+    EditText content_editText;
+    EditText title_editText;
+    Spinner type_spinner;
+
+    public interface add_event_dialog_result{
+        void get_result(String type, String title, String content, int color);
+    }
+    public void setDialogResult(add_event_dialog_result dialogResult)
+    {
+        mdialogResult = dialogResult;
+    }
+
+    public add_event_dialog()
+    {
+
+    }
+
+    public add_event_dialog(Context tmp_context)
+    {
+        context = tmp_context;
+    }
+
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View rootView = inflater.inflate(R.layout.add_event_dialog, null);
+        builder.setView(rootView);
+
+        title_editText = rootView.findViewById(R.id.add_event_title);
+        content_editText = rootView.findViewById(R.id.add_event_content);
+        type_spinner = rootView.findViewById(R.id.type_spinner);
+
+        // type spinner
+        ArrayList<String> type_items = new ArrayList<>();
+        type_items.add("휴가");
+        type_items.add("근무");
+        type_items.add("훈련");
+
+        ArrayAdapter<String> category_adapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, type_items);
+        //category_adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        type_spinner.setAdapter(category_adapter);
+        type_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        //color picker
+        rootView.findViewById(R.id.color_picker).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new ColorPickerPopup.Builder(context)
+                        .initialColor(Color.BLACK) // Set initial color
+                        .enableAlpha(true) // Enable alpha slider or not
+                        .okTitle("Choose")
+                        .cancelTitle("Cancel")
+                        .showIndicator(true)
+                        .showValue(true)
+                        .build()
+                        .show(rootView, new ColorPickerPopup.ColorPickerObserver() {
+                            @Override
+                            public void onColorPicked(int color) {
+                                rootView.findViewById(R.id.color_picker).setBackgroundColor(color);
+                                selected_color = color;
+                            }
+
+                            @Override
+                            public void onColor(int color, boolean fromUser) {
+
+                            }
+                        });
+
+
+            }
+        });
+
+        // click listener 연결
+        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                mdialogResult.get_result(type_spinner.getSelectedItem().toString(), title_editText.getText().toString(), content_editText.getText().toString(), selected_color);
+                dismiss();
+            }
+        })
+                .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dismiss();
+                    }
+                });
+
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
